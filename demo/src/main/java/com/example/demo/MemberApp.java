b234c411ec5478e77a62e55ecc0df134f3cba32b
hyomin911 <hyomin911@naver.com>
2025-02-18 16:40:27 +0900

    요기까지..

--- demo/src/main/java/com/example/demo/MemberApp.java
+++ demo/src/main/java/com/example/demo/MemberApp.java
@@ -5,11 +5,20 @@ import com.example.demo.member.Grade;
 import com.example.demo.member.Member;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
+import com.example.demo.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        //  AppConfig appConfig = new AppConfig();
+      //  MemberService memberService = appConfig.memberService();
+       // MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- demo/src/test/java/com/example/demo/member/MemberServiceTest.java
+++ demo/src/test/java/com/example/demo/member/MemberServiceTest.java
@@ -1,11 +1,20 @@
 package com.example.demo.member;
 
+import com.example.demo.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
